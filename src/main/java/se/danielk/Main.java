f890620362951f925b939720013b2f3edd6023b7
Daniel.Kahma <you@example.com>
2024-10-10 11:21:47 +0200

    finished the calculator

--- src/main/java/se/danielk/Main.java
+++ src/main/java/se/danielk/Main.java
@@ -7,51 +7,64 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int operator, num1, num2;
+        int operator;
         Scanner scanner = new Scanner(System.in);
-        double number1, number2;
+        String answer;
+        do {
+            double number1, number2;
 
-        System.out.println("--------------------");
-        System.out.println("Daniels Calculator!");
-        System.out.println("--------------------");
+            System.out.println("--------------------");
+            System.out.println("Daniels Calculator!");
+            System.out.println("--------------------");
 
-        System.out.println("Enter your first number:");
-        number1 = scanner.nextInt();
+            System.out.println("Enter your first number:");
+            number1 = scanner.nextInt();
 
-        System.out.println("Choose between: ");
-        System.out.println("1.+");
-        System.out.println("2.-");
-        System.out.println("3.*");
-        System.out.println("4./");
+            System.out.println("Choose between: ");
+            System.out.println("1.+");
+            System.out.println("2.-");
+            System.out.println("3.*");
+            System.out.println("4./");
 
-        operator = scanner.nextInt();
+            operator = scanner.nextInt();
 
-        System.out.println("Enter your second number: ");
-        number2 = scanner.nextInt();
+            System.out.println("Enter your second number: ");
+            number2 = scanner.nextInt();
 
-        double sum = 0;
+            double sum = 0;
 
-        switch (operator) {
-            case 1: sum = number1 + number2;
-            break;
+            switch (operator) {
+                case 1:
+                    sum = number1 + number2;
+                    break;
 
-            case 2: sum = number1 - number2;
-            break;
+                case 2:
+                    sum = number1 - number2;
+                    break;
 
-            case 3: sum = number1 * number2;
-                break;
+                case 3:
+                    sum = number1 * number2;
+                    break;
 
-            case 4: sum = number1 / number2;
-                break;
+                case 4:
+                    sum = number1 / number2;
+                    break;
 
-            default:
-                System.out.println("Invalid option.");
+                default:
+                    System.out.println("Invalid option.");
+            }
+
+            System.out.println("Answer: " + sum);
+
+
+            System.out.println("Want yo calculate more? yes or no");
+            answer = scanner.next();
+
         }
-
-        System.out.println("Answer: " + sum);
-
-
-        }
+        while (answer.equals("yes"));
 
 
     }
+
+
+}
